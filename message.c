e3f177dc137e256e80f78045db4f8aea3630efa8
Abhishek Kumar Verma <40899651+v-abhi@users.noreply.github.com>
2018-11-19 09:28:39 +0530

    Add files via upload

---
+++ message.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main( int no_of_passwords, char *passwords[] )
+{
+
+	
+	if( !strcmp(passwords[1],"password_1"))
+	{
+		
+		printf("your message is here.\n");
+	}
+	else
+	{
+		printf("sorry\n");
+	}
+	
+return 0;
+	
+}
